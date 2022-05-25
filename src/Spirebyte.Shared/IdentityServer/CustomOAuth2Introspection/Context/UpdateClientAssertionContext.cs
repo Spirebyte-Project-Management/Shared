474daec94c7725dd1d9cd0c58f1d9da345bef6bf
Jordi Huntjens <442216@student.fontys.nl>
2022-05-25 17:52:02 +0200

    Fix build

---
+++ src/Spirebyte.Shared/IdentityServer/CustomOAuth2Introspection/Context/UpdateClientAssertionContext.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Dominick Baier & Brock Allen. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityModel.AspNetCore.OAuth2Introspection;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace Spirebyte.Shared.IdentityServer.CustomOAuth2Introspection.Context
+{
+    /// <summary>
+    /// Context for the UpdateClientAssertion event
+    /// </summary>
+    public class UpdateClientAssertionContext : ResultContext<ExtendedOAuth2IntrospectionOptions>
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public UpdateClientAssertionContext(
+            HttpContext context,
+            AuthenticationScheme scheme,
+            ExtendedOAuth2IntrospectionOptions options)
+            : base(context, scheme, options) { }
+
+        /// <summary>
+        /// The client assertion
+        /// </summary>
+        public ClientAssertion ClientAssertion { get; set; }
+
+        /// <summary>
+        /// The client assertion expiration time
+        /// </summary>
+        public DateTime ClientAssertionExpirationTime { get; set; }
+    }
+}
---
+++ src/Spirebyte.Shared/IdentityServer/CustomOAuth2Introspection/Infrastructure/CacheExtensions.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Dominick Baier & Brock Allen. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using IdentityModel;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Spirebyte.Shared.IdentityServer.CustomOAuth2Introspection.Infrastructure
+{
+    internal static class CacheExtensions
+    {
+        private static readonly JsonSerializerOptions Options;
+
+        static CacheExtensions()
+        {
+            
+#if NET6_0_OR_GREATER
+            Options = new JsonSerializerOptions
+            {
+                IgnoreReadOnlyFields = true,
+                IgnoreReadOnlyProperties = true,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
+#else
+            Options = new JsonSerializerOptions
+            {
+                IgnoreReadOnlyFields = true,
+                IgnoreReadOnlyProperties = true,
+                IgnoreNullValues = true
+            };
+#endif
+            
+            Options.Converters.Add(new ClaimConverter());
+        }
+
+        public static async Task<IEnumerable<Claim>> GetClaimsAsync(this IDistributedCache cache, ExtendedOAuth2IntrospectionOptions options, string token)
+        {
+            var cacheKey = options.CacheKeyGenerator(options,token);
+            var bytes = await cache.GetAsync(cacheKey).ConfigureAwait(false);
+
+            if (bytes == null)
+            {
+                return null;
+            }
+
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonSerializer.Deserialize<IEnumerable<Claim>>(json, Options);
+        }
+
+        public static async Task SetClaimsAsync(this IDistributedCache cache, ExtendedOAuth2IntrospectionOptions options, string token, IEnumerable<Claim> claims, TimeSpan duration, ILogger logger)
+        {
+            var expClaim = claims.FirstOrDefault(c => c.Type == JwtClaimTypes.Expiration);
+            if (expClaim == null)
+            {
+                logger.LogWarning("No exp claim found on introspection response, can't cache.");
+                return;
+            }
+
+            var now = DateTimeOffset.UtcNow;
+            var expiration = DateTimeOffset.FromUnixTimeSeconds(long.Parse(expClaim.Value));
+            logger.LogDebug("Token will expire in {expiration}", expiration);
+
+
+            if (expiration <= now)
+            {
+                return;
+            }
+
+            // if the lifetime of the token is shorter than the duration, use the remaining token lifetime
+            DateTimeOffset absoluteLifetime;
+            if (expiration <= now.Add(duration))
+            {
+                absoluteLifetime = expiration;
+            }
+            else
+            {
+                absoluteLifetime = now.Add(duration);
+            }
+
+            var json = JsonSerializer.Serialize(claims, Options);
+            var bytes = Encoding.UTF8.GetBytes(json);
+
+            logger.LogDebug("Setting cache item expiration to {expiration}", absoluteLifetime);
+            var cacheKey = options.CacheKeyGenerator(options, token);
+            await cache.SetAsync(cacheKey, bytes, new DistributedCacheEntryOptions { AbsoluteExpiration = absoluteLifetime }).ConfigureAwait(false);
+        }
+    }
+}
