cda2383791a63857aa61d3da6abdc365c57507f0
Jordi Huntjens <442216@student.fontys.nl>
2022-05-25 15:47:57 +0200

    Update shared

--- src/Spirebyte.Shared/IdentityServer/Extensions.cs
+++ src/Spirebyte.Shared/IdentityServer/Extensions.cs
@@ -18,7 +18,7 @@ public static class Extensions
 
     
     public static IConveyBuilder AddIdentityServerAuthentication(this IConveyBuilder builder, string sectionName = SectionName,
-        Action<JwtBearerOptions> optionsFactory = null)
+        Action<JwtBearerOptions> optionsFactory = null, bool withBasic = false)
     {
         if (string.IsNullOrWhiteSpace(sectionName))
         {
@@ -26,7 +26,7 @@ public static class Extensions
         }
 
         var options = builder.GetOptions<JwtOptions>(sectionName);
-        return builder.AddIdentityServerAuthentication(options, optionsFactory);
+        return withBasic ? builder.AddIdentityServerAuthenticationWithBasicToken(options, optionsFactory) : builder.AddIdentityServerAuthentication(options, optionsFactory);
     }
     
     private static IConveyBuilder AddIdentityServerAuthentication(this IConveyBuilder builder, JwtOptions options,
@@ -102,7 +102,112 @@ public static class Extensions
 
         return builder;
     }
+    
+    private static IConveyBuilder AddIdentityServerAuthenticationWithBasicToken(this IConveyBuilder builder, JwtOptions options,
+        Action<JwtBearerOptions> optionsFactory = null)
+    {
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            RequireAudience = options.RequireAudience,
+            ValidIssuer = options.ValidIssuer,
+            ValidIssuers = options.ValidIssuers,
+            ValidateActor = options.ValidateActor,
+            ValidAudience = options.ValidAudience,
+            ValidAudiences = options.ValidAudiences,
+            ValidateAudience = options.ValidateAudience,
+            ValidateIssuer = options.ValidateIssuer,
+            ValidateLifetime = options.ValidateLifetime,
+            ValidateTokenReplay = options.ValidateTokenReplay,
+            ValidateIssuerSigningKey = options.ValidateIssuerSigningKey,
+            SaveSigninToken = options.SaveSigninToken,
+            RequireExpirationTime = options.RequireExpirationTime,
+            RequireSignedTokens = options.RequireSignedTokens,
+            ClockSkew = TimeSpan.Zero
+        };
 
+        if (!string.IsNullOrWhiteSpace(options.AuthenticationType))
+        {
+            tokenValidationParameters.AuthenticationType = options.AuthenticationType;
+        }
+
+        if (!string.IsNullOrWhiteSpace(options.NameClaimType))
+        {
+            tokenValidationParameters.NameClaimType = options.NameClaimType;
+        }
+
+        if (!string.IsNullOrWhiteSpace(options.RoleClaimType))
+        {
+            tokenValidationParameters.RoleClaimType = options.RoleClaimType;
+        }
+        
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        
+        builder.Services.AddAuthentication("token")
+ 
+            // JWT tokens (default scheme)
+            .AddJwtBearer("token", o =>
+            {
+                o.Authority = options.Authority;
+                o.Audience = options.Audience;
+                o.MetadataAddress = options.MetadataAddress;
+                o.SaveToken = options.SaveToken;
+                o.RefreshOnIssuerKeyNotFound = options.RefreshOnIssuerKeyNotFound;
+                o.RequireHttpsMetadata = options.RequireHttpsMetadata;
+                o.IncludeErrorDetails = options.IncludeErrorDetails;
+                o.TokenValidationParameters = tokenValidationParameters;
+                if (!string.IsNullOrWhiteSpace(options.Challenge))
+                {
+                    o.Challenge = options.Challenge;
+                }
+
+                optionsFactory?.Invoke(o);
+                
+                o.ForwardDefaultSelector = ForwardReferenceTokenWithBasic("introspection", "basic-introspection");
+            })
+ 
+            // reference tokens
+            .AddOAuth2Introspection("introspection", o =>
+            {
+                o.Authority = options.Authority;
+                
+                o.ClientId = options.ClientId;
+                o.ClientSecret = options.ClientSecret;
+            })
+            // reference tokens
+            .AddOAuth2Introspection("basic-introspection", o =>
+            {
+                o.TokenRetriever = TokenRetrieval.BasicFromAuthorizationHeader();
+                
+                o.Authority = options.Authority;
+                o.ClientId = options.ClientId;
+                o.ClientSecret = options.ClientSecret;
+            });
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Provides a forwarding func for JWT vs reference tokens (based on existence of dot in token)
+    /// </summary>
+    /// <param name="introspectionScheme">Scheme name of the introspection handler</param>
+    /// <returns></returns>
+    public static Func<HttpContext, string> ForwardReferenceTokenWithBasic(string introspectionScheme = "Introspection", string basicIntrospectionScheme = "Basic-introspection")
+    {
+        return context =>
+        {
+            var (str3, str4) = Selector.GetSchemeAndCredential(context);
+            if (str3.Equals("Bearer", StringComparison.OrdinalIgnoreCase) && !str4.Contains("."))
+            {
+                return introspectionScheme;
+            }
+            if (str3.Equals("Basic", StringComparison.OrdinalIgnoreCase) && !str4.Contains(":"))
+            {
+                return basicIntrospectionScheme;
+            }
+            return string.Empty;
+        };
+    }
+    
     public static AuthorizationOptions AddEitherOrScopePolicy(this AuthorizationOptions options, string policyName, string firstScope, string secondScope)
     {
         options.AddPolicy(policyName, p =>
---
+++ src/Spirebyte.Shared/IdentityServer/TokenRetrieval.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Spirebyte.Shared.IdentityServer;
+
+public static class TokenRetrieval
+{
+    public static Func<HttpRequest, string> BasicFromAuthorizationHeader(string scheme = "Basic")
+    {
+        return request =>
+        {
+            string authorization = request.Headers["Authorization"].FirstOrDefault();
+            
+            if (string.IsNullOrEmpty(authorization))
+            {
+                return null;
+            }
+
+            if (authorization.StartsWith(scheme + " ", StringComparison.OrdinalIgnoreCase))
+            {
+                return authorization.Substring(scheme.Length + 1).Trim();
+            }
+            
+            var authBase64 = Encoding.UTF8.GetString(Convert.FromBase64String(authorization));
+            var authSplit = authBase64.Split(Convert.ToChar(":"), 2);
+            if (authSplit.Length > 1)
+            {
+                return authSplit[1];
+            }
+
+            return null;
+        };
+    }
+
+    /// <summary>
+    /// Reads the token from a query string parameter.
+    /// </summary>
+    /// <param name="name">The name (defaults to access_token).</param>
+    /// <returns></returns>
+    public static Func<HttpRequest, string> FromQueryString(string name = "access_token")
+    {
+        return request => request.Query[name].FirstOrDefault();
+    }
+}
